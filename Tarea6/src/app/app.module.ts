c1c4a3c44b72b71c1f1f9629892cdfd8e2539908
lauraggc <laura.gonzalez@iteso.mx>
2023-03-29 07:43:37 -0600

    create components

--- Tarea6/src/app/app.module.ts
+++ Tarea6/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EducationComponent } from './education/education.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    EducationComponent,
+    CoursesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
